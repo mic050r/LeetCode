d4be448462b7cd7b21484cf0a078bedc27cac522
林지현 <103114387+mic050r@users.noreply.github.com>
2024-02-03 23:46:36 +0900

    Time: 7 ms (58.48%), Space: 42.2 MB (30.35%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int[] dp = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int max = arr[i];
+            for (int j = 1; j <= k && i - j + 1 >= 0; j++) {
+                max = Math.max(max, arr[i - j + 1]);
+                dp[i] = Math.max(dp[i], (i >= j ? dp[i - j] : 0) + max * j);
+            }
+        }
+        
+        return dp[n - 1];
+    }
+}
